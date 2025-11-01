1db733d884a4b8bab045a13152ed7b127f25b74c
AR-2013 <anairarandeva@icloud.com>
2025-11-01 10:36:19 +0400

    Homework

---
+++ Homework.py
@@ -0,0 +1,7 @@
+age = int(input("Enter your age in numbers: "))
+if age < 10:
+    print("You are too young!")
+elif age > 20:
+    print("You are too old!")
+else:
+    print("You are the right age!")
